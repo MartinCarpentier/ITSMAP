bc85e912027f3af636af61c5ddae435583417dcf
Joachim Andersen <joachimdamandersen@gmail.com>
2017-05-05 10:30:53 +0200

    implemented FeedReaderContract and FeedReaderHelper

---
+++ WeatherDatabase/app/src/main/java/com/example/jodaa/weatherdatabase/FeedReaderContract.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Jodaa on 05-05-2017.
+ * Source used:
+ * https://developer.android.com/training/basics/data-storage/databases.html
+ */
+package com.example.jodaa.weatherdatabase;
+import android.provider.BaseColumns;
+
+public final class FeedReaderContract {
+    private FeedReaderContract(){}
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + FeedEntry.TABLE_NAME + " (" +
+                    FeedEntry._ID + " INTEGER PRIMARY KEY," +
+                    FeedEntry.COLUMN_NAME_TEMP + " TEXT," +
+                    FeedEntry.COLUMN_NAME_TIME + " TEXT)";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + FeedEntry.TABLE_NAME;
+
+
+    public static class FeedEntry implements BaseColumns{
+        public static final String TABLE_NAME = "WeatherData";
+        public static final String COLUMN_NAME_TEMP = "Temperature";
+        public static final String COLUMN_NAME_TIME = "Time";
+    }
+}
