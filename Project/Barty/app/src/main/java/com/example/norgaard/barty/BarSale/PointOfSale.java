2e7bca084d3b5edfd60fd5d950f7825d924132b8
Joachim Andersen <joachimdamandersen@gmail.com>
2017-05-25 15:39:57 +0200

    populated spinner with payment methods

--- Project/Barty/app/src/main/java/com/example/norgaard/barty/BarSale/PointOfSale.java
+++ Project/Barty/app/src/main/java/com/example/norgaard/barty/BarSale/PointOfSale.java
@@ -1,11 +1,12 @@
 package com.example.norgaard.barty.BarSale;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.ButtonBarLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import com.example.norgaard.barty.R;
 
@@ -18,17 +19,29 @@ public class PointOfSale extends AppCompatActivity {
 
     private Button goToPayment;
     private int MOBILEPAY_REQUEST_CODE = 999;
+    private Spinner paymentMethods;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_point_of_sale);
-
         goToPayment = (Button) findViewById(R.id.btnGoToPayment);
+
+
+        // Code taken from/inspired  by:
+        // https://developer.android.com/guide/topics/ui/controls/spinner.html
+        paymentMethods = (Spinner) findViewById(R.id.spinnerPaymentMethod);
+        ArrayAdapter<CharSequence> paymentMethodsArrayAdapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.payment_methods,
+                android.R.layout.simple_spinner_item);
+
+        paymentMethodsArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        paymentMethods.setAdapter(paymentMethodsArrayAdapter);
         goToPayment.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // https://github.com/MobilePayDev/MobilePay-AppSwitch-SDK/wiki/Getting-started-on-Android
 
+                // https://github.com/MobilePayDev/MobilePay-AppSwitch-SDK/wiki/Getting-started-on-Android
                 boolean isMobilePayInstalled = MobilePay.getInstance().isMobilePayInstalled(getApplicationContext());
                 if (isMobilePayInstalled) {
                     Payment payment = new Payment();
