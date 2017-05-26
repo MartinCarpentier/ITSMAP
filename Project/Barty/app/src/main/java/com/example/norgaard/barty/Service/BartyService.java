f0ac68b09743c1e278d7d8856f9de74704e460dc
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-05-26 12:57:38 +0200

    Added empty methods

--- Project/Barty/app/src/main/java/com/example/norgaard/barty/Service/BartyService.java
+++ Project/Barty/app/src/main/java/com/example/norgaard/barty/Service/BartyService.java
@@ -3,6 +3,7 @@ package com.example.norgaard.barty.Service;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.support.annotation.IntDef;
 
 public class BartyService extends Service {
 
@@ -13,5 +14,20 @@ public class BartyService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, @IntDef(value = {Service.START_FLAG_REDELIVERY, Service.START_FLAG_RETRY}, flag = true) int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 }
