57189a2b379b5943000316940bc46e92f2088ff1
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-05-26 13:01:24 +0200

    Added logging

--- Project/Barty/app/src/main/java/com/example/norgaard/barty/Service/BartyService.java
+++ Project/Barty/app/src/main/java/com/example/norgaard/barty/Service/BartyService.java
@@ -3,31 +3,43 @@ package com.example.norgaard.barty.Service;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
-import android.support.annotation.IntDef;
+import android.util.Log;
 
 public class BartyService extends Service {
 
+    public static final String SERVICE = "SERVICE";
+    public static final String BARTY_SERVICE = "BartyService";
+
     public BartyService() {
+        Log.d(SERVICE, BARTY_SERVICE + " in Constructor");
     }
 
     @Override
     public IBinder onBind(Intent intent) {
+        Log.d(SERVICE, BARTY_SERVICE + " in onBind()");
+
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
     @Override
     public void onCreate() {
+        Log.d(SERVICE, BARTY_SERVICE + " in onCreate()");
+
         super.onCreate();
     }
 
     @Override
-    public int onStartCommand(Intent intent, @IntDef(value = {Service.START_FLAG_REDELIVERY, Service.START_FLAG_RETRY}, flag = true) int flags, int startId) {
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(SERVICE, BARTY_SERVICE + " in onStartCommand()");
+
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
+        Log.d(SERVICE, BARTY_SERVICE + " in onDestroy");
+
         super.onDestroy();
     }
 }
