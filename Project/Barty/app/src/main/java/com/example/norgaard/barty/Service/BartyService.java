6e227035e2ce1bddd33dc3b09912026b5995c044
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-05-28 18:18:50 +0200

    Service now works in background, only when app already has been started

--- Project/Barty/app/src/main/java/com/example/norgaard/barty/Service/BartyService.java
+++ Project/Barty/app/src/main/java/com/example/norgaard/barty/Service/BartyService.java
@@ -5,42 +5,55 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class BartyService extends Service {
 
-    // For logging purposes
-    public static final String LOG_MESSAGE_START = "BartyService ";
-    public static final String LOG_TAG = "LOG_TAG";
+    // Logging
+    public static final String LOG_TAG = "BartyServiceLogTag";
+
+    // Timer
+    private LoopRequest loopRequest;
+    private long delay = 2 * 1000;
+    private static Timer timer;
 
     private boolean isServiceStarted;
 
     public BartyService() {
-        Log.d(LOG_TAG, LOG_MESSAGE_START + "constructor");
+        Log.d(LOG_TAG, "Constructor invoked");
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        Log.d(LOG_TAG, LOG_MESSAGE_START + "onBind()");
+        Log.d(LOG_TAG, "onBind()");
         return null;
     }
 
     @Override
     public void onCreate() {
-        Log.d(LOG_TAG, LOG_MESSAGE_START + "onCreate()");
+        Log.d(LOG_TAG, "onCreate()");
         super.onCreate();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d(LOG_TAG, LOG_MESSAGE_START + "onStartCommand()");
+        Log.d(LOG_TAG, "onStartCommand()");
 
         if (isServiceStarted == false && intent != null) {
-            Log.d(LOG_TAG, LOG_MESSAGE_START + "service started");
+            Log.d(LOG_TAG, "Service started");
             isServiceStarted = true;
 
-            // Do stuff
+            loopRequest = new LoopRequest();
+            timer = new Timer();
+            //timer.cancel();
+            Date date = new Date();
+            timer.scheduleAtFixedRate(loopRequest, date, delay);
         }
         else {
-            Log.d(LOG_TAG, LOG_MESSAGE_START + "already started");
+            Log.d(LOG_TAG, "Service already started");
         }
 
         return START_STICKY;
@@ -48,8 +61,16 @@ public class BartyService extends Service {
 
     @Override
     public void onDestroy() {
-        Log.d(LOG_TAG, LOG_MESSAGE_START + "onDestroy");
+        Log.d(LOG_TAG, "onDestroy");
         isServiceStarted = false;
         super.onDestroy();
+    }
+
+    private class LoopRequest extends TimerTask {
+
+        @Override
+        public void run() {
+            Log.d(LOG_TAG, "Run method has been called! " + Calendar.getInstance().getTime().toString());
+        }
     }
 }
