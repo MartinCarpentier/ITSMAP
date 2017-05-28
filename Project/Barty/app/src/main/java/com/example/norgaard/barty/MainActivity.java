9ba7b62b9541073f0bd8716209f63adba393d376
Joachim Andersen <joachimdamandersen@gmail.com>
2017-05-29 01:08:36 +0200

    signIn is now handled in code

--- Project/Barty/app/src/main/java/com/example/norgaard/barty/MainActivity.java
+++ Project/Barty/app/src/main/java/com/example/norgaard/barty/MainActivity.java
@@ -6,25 +6,40 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.awareness.state.Weather;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-
-import static android.R.attr.data;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends FragmentActivity {
 
     private static final int RC_SIGN_IN = 101;
     private GoogleApiClient mGoogleApiClient;
+    private FirebaseAuth firebaseAuth;
+    private TextView currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        currentUser = (TextView) findViewById(R.id.textViewCurrentUser);
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         // The following is heavily inspired by
         // https://developers.google.com/identity/sign-in/android/sign-in
@@ -33,6 +48,7 @@ public class MainActivity extends FragmentActivity {
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
+                .requestIdToken(getString(R.string.oauth2_secret_key))
                 .build();
 
         // Build a GoogleApiClient with access to the Google Sign-In API and the
@@ -57,6 +73,13 @@ public class MainActivity extends FragmentActivity {
 
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        updateUser(currentUser);
+    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -69,18 +92,56 @@ public class MainActivity extends FragmentActivity {
         }
     }
 
-
     private void handleSignInResult(GoogleSignInResult result) {
         Log.d(this.getClass().getSimpleName(), "handleSignInResult:" + result.isSuccess());
         if (result.isSuccess()) {
             // Signed in successfully, show authenticated UI.
             GoogleSignInAccount acct = result.getSignInAccount();
+            firebaseAuthWithGoogle(acct);
+
 
         } else {
             // Signed out, show unauthenticated UI.
+            Toast.makeText(this.getApplicationContext(), R.string.info_not_signed_in_key, Toast.LENGTH_SHORT).show();
+
         }
     }
 
+    private void updateUser(FirebaseUser user) {
+
+        if (user == null){
+            currentUser.setText("Guest");
+        }
+        else {
+            currentUser.setText(user.getDisplayName());
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(this.getClass().getSimpleName(), "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(this.getClass().getSimpleName(), "signInWithCredential:success");
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            updateUser(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(this.getClass().getSimpleName(), "signInWithCredential:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUser(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
 }
 
 
