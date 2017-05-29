4d194d7d29a4029ebff30e80da8761f31714ee7d
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-05-29 16:01:32 +0200

    Minor change

--- Project/functions/index.js
+++ Project/functions/index.js
@@ -1,8 +1,11 @@
+// From firebase sample code
 var functions = require('firebase-functions');
+
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase!");
 });
@@ -11,11 +14,10 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 // Realtime Database under the path /messages/:pushId/original
 exports.addMessage = functions.https.onRequest((req, res) => {
     // Grab the text parameter.
-    const original = req.query.text;
+    var order = JSON.parse(req.query.order);
+
     // Push it into the Realtime Database then send a response
-    admin.database().ref('/messages').push({original: original}).then(snapshot => {
-        // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-        res.redirect(303, snapshot.ref);
-    });
+    admin.database().ref('/derps').push({post: original});
+
+    res.send(order + "");
 });
-index.js
